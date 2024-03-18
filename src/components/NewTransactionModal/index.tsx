c3fb62816bc490e9e5be81a7c577607cb04f20e0
Wanderson Chaves <wandersonchaves@goapice.com>
2024-03-18 20:18:28 -0300

    first commit and add json-server

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,43 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <Overlay />
+      <Content>
+        <Dialog.Title>Nova transacao</Dialog.Title>
+
+        <CloseButton>
+          <X />
+        </CloseButton>
+
+        <form action="">
+          <input type="text" placeholder="Descricao" required />
+          <input type="number" placeholder="Preco" required />
+          <input type="text" placeholder="Categoria" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
+
+          <button type="submit">Cadastrar</button>
+        </form>
+      </Content>
+    </Dialog.Portal>
+  );
+}
---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,44 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+
+        <TransactionsTable>
+          <thead>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/02/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 12,00</PriceHighlight>
+              </td>
+              <td>Alimentacao</td>
+              <td>13/02/2022</td>
+            </tr>
+          </thead>
+
+          <tbody></tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  );
+}
